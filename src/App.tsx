10ba480301d7fee53e8e7295aa344e4ba48c3236
Bharadwaj Kura <kurashiva11@gmail.com>
2022-04-05 12:28:38 +0530

    Add routes to initail page and no match page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router';
+
+import About from './screens/About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<About />} />
+        {/* No Match route */}
+        {/* TODO: Show that there is nothing here to learn. */}
+        <Route path="*" element={<About />} />
+      </Routes>
     </div>
   );
 }
