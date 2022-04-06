70e268155ef96ab7ed191fe738fa2d7b408747a3
Bharadwaj Kura <kurashiva11@gmail.com>
2022-04-06 16:16:02 +0530

    Fix use_scroll_effect flaw

--- src/hooks/useScrollEffect.tsx
+++ src/hooks/useScrollEffect.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useMemo } from "react";
-import { debounce } from 'lodash';
+import { useCallback, useEffect, useMemo } from "react";
+import { throttle } from 'lodash';
 
 import { screenScrollTime } from '../constants';
 
@@ -13,18 +13,23 @@ type scrollEffectOptions = {
 const RESISTANCE = 40;
 
 const useScrollEffect = (options: scrollEffectOptions) => {
-    const debouncedScrollTop = useMemo(() => debounce(options.onScrollTop, screenScrollTime, {leading: true, trailing: false}), []);
-    const debouncedScrollBottom = useMemo(() => debounce(options.onScrollBottom, screenScrollTime, {leading: true, trailing: false}), []);
+    const debouncedScrollTop = useMemo(() => throttle(options.onScrollTop, screenScrollTime, {trailing: false}), []);
+    const debouncedScrollBottom = useMemo(() => throttle(options.onScrollBottom, screenScrollTime, {trailing: false}), []);
+
+    const onScroll = useCallback((event: any) => {
+        // whellDelta is difference between the previous scroll and current scroll, so can be used to determine the scroll direction.
+        if (event.wheelDelta > RESISTANCE) {
+            debouncedScrollTop();
+        } else if (event.wheelDelta < -RESISTANCE) {
+            debouncedScrollBottom();
+        }
+    }, []);
 
     useEffect(() => {
-        document.addEventListener('wheel', (event: any) => {
-            // whellDelta is difference between the previous scroll and current scroll, so can be used to determine the scroll direction.
-            if (event.wheelDelta > RESISTANCE) {
-                debouncedScrollTop();
-            } else if (event.wheelDelta < -RESISTANCE) {
-                debouncedScrollBottom();
-            }
-        });
+        document.addEventListener('wheel', onScroll, false);
+        
+        // remove the previous screen's event listener to avoid multiple attachment of listeners
+        return () => document.removeEventListener('wheel', onScroll, false);
     }, []);
 }
 
