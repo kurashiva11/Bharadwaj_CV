1777c88c6a4ec55ea33d31b60cf2102de8e0a5f2
Bharadwaj Kura <kurashiva11@gmail.com>
2022-04-08 11:16:39 +0530

    about screen css changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 
@@ -13,3 +13,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+/** TODO:
+ * 1. show mutual attraction at contact page (https://www.youtube.com/watch?v=GjbKsOkN1Oc).
+ * 2. show starfield till the page loads (https://www.youtube.com/watch?v=17WoOqgXsRM&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=1).
+ * 
+ * color blast
+ * bullseye (codepen: https://codepen.io/pyrografix/pen/qrqpJN?editors=0110)
+ * planet defence (learn from https://www.youtube.com/watch?v=eI9idPTT0c4) (output similar to: https://codepen.io/Loopez10/pen/dMaVdQ)
+ * coloron
+ */
--- src/screens/about/index.tsx
+++ src/screens/about/index.tsx
@@ -1,19 +1,36 @@
-import { useEffect } from 'react';
-
+import { useEffect, useRef } from 'react';
 import withMountAnimatedScreen from '../../HOC/withMountAnimatedScreen';
 import styles from './about.module.scss';
 
 function About() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
     useEffect(() => {
-        console.log('About mounted');
-        return () => {
-            console.log('removing about');
+        var ctx = canvasRef.current?.getContext("2d");
+        if (ctx) {
+            ctx.moveTo(0, 0);
+            ctx.lineTo(150, 80);
+            ctx.stroke();
         }
-    }, [])
+    }, []);
 
     return (
         <div className={styles.about}>
-            About Page.
+            <div className={styles.introduction}>
+                <div className={styles["introduction-name"]}>Bharadwaj Kura.</div>
+                <div className={styles["introduction-resignation"]}>Software Developer.</div>
+            </div>
+            <div className={styles["game_comtainer"]}>
+                game
+                <canvas
+                    ref={canvasRef}
+                    width="200"
+                    height="100"
+                    style={{ border: "1px solid #d3d3d3" }}
+                >
+                    Your browser does not support the HTML canvas tag.
+                </canvas>
+            </div>
         </div>
     );
 }
