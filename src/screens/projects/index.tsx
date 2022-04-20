d02b93c9c2606156c662d5b64b0672ca1df54e8d
Bharadwaj Kura <kurashiva11@gmail.com>
2022-04-20 15:51:39 +0530

    Add grid layout

--- src/screens/projects/index.tsx
+++ src/screens/projects/index.tsx
@@ -6,11 +6,48 @@ type Props = {
     isActive: boolean;
 }
 
+const projectsData = [
+    {
+        title: '1st',
+        describe: 'this is first',
+        uri: 'google.com/'
+    },
+    {
+        title: '1st',
+        describe: 'this is first',
+        uri: 'google.com/'
+    },
+    {
+        title: '1st',
+        describe: 'this is first',
+        uri: 'google.com/'
+    },
+    {
+        title: '1st',
+        describe: 'this is first',
+        uri: 'google.com/'
+    },
+    {
+        title: '1st',
+        describe: 'this is first',
+        uri: 'google.com/'
+    },
+]
+
 
 function Projects(props: Props) {
     return (
         <div className={styles['projects']}>
-            projects
+            <ul className={styles["grid"]}>
+                {[...projectsData].map((project) => {
+                    return (
+                        <li onClick={()=> window.open(project.uri, "_blank")}>
+                            <div className={styles["title"]}>{project.title}</div>
+                            <span className={styles["description"]}>{project.describe}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
