b2b2684d7e095b5db1f82642db2bda28cd6a4175
Bharadwaj Kura <kurashiva11@gmail.com>
2022-04-21 13:05:26 +0530

    background chnages

--- src/screens/skills/index.tsx
+++ src/screens/skills/index.tsx
@@ -1,15 +1,40 @@
+import { useEffect } from 'react';
 import styles from './index.module.scss';
 
 import withMountAnimatedScreen from '../../HOC/withMountAnimatedScreen';
+
+import TagCloudJS from './TagCloug';
 
 type Props = {
     isActive: boolean;
 }
 
+const technicalSkills = [
+    'JavaScript', 'Java', 'C++',
+    'Python', 'Reactjs', 'React Native',
+    'Django', 'HTML/CSS', 'Docker',
+    'Git', 'Node JS', 'MySQL',
+    'MongoDB',
+];
+
 function Skills(props: Props) {
+
+    useEffect(() => {
+        TagCloudJS("." + styles['Sphere'], technicalSkills, {
+            radius: 250,
+            maxSpeed: 'fast',
+            initSpeed: 'fast',
+            direction: 135,
+            keep: true,
+        })
+    }, []);
+
     return (
         <div className={styles['skills']}>
-            skills
+            <div className={styles["container"]}>
+                <span className={styles["Sphere"]}></span>
+            </div>
+
             <div className={styles["area"]} >
                 <ul className={styles["circles"]}>
                     {Array(15).fill(0).map(() => <li></li>)}
